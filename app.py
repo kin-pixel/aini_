adb1388db99a617def7c317899491ccc79719296
kin-pixel <kintanaa123@gmail.com>
2025-04-28 20:41:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return render_template('home.html')
+    return render_template('Home.html')
 
 
 @app.route("/gallery")
@@ -15,7 +15,7 @@ def gallery():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('About.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
